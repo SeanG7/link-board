6aa4fcd63834f65b279c6e52b065e230b3aac828
Sean <gilmore.seven@gmail.com>
2017-06-10 22:31:25 +1000

    add create, show and index actions

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,9 +1,25 @@
+# frozen_string_literal: true
+# The boards controller
 class BoardsController < ApplicationController
+  def index
+    @boards = Board.all
+  end
 
-def index
-end
+  def create
+    @board = Board.new(board_params)
+    @board.save
+    redirect_to @board
+  end
 
-def new
-end
+  def new; end
 
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:name)
+  end
 end
