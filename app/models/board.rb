a4b68fd32f3a877a8ad1dd3f3d0a38daf73cdd63
Sean <gilmore.seven@gmail.com>
2017-06-11 00:25:27 +1000

    add many to one relationship
    many links belong to a single board

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+# Board model
 class Board < ApplicationRecord
+  has_many :links, dependent: :destroy
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+# Link resource
 class Link < ApplicationRecord
+  # Each link belongs to a Board
+  belongs_to :board
 end
